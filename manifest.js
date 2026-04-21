93958264736b9f95ecc5c1a068742c523156d134
vanseleem <98992832+vanseleem@users.noreply.github.com>
2026-04-21 13:14:04 +0200

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,8 +1,8 @@
 module.exports = {
   id: "org.ghostream.platinum",
-  version: "1.6.0",
+  version: "1.7.0",
   name: "Ghostream Platinum \uD83D\uDE80",
-  description: "720p/1080p High-Seeder Indexer (YTS, EZTV, 1337x)",
+  description: "720p/1080p High-Seeder Indexer (YTS, EZTV, SolidTorrents)",
   resources: ["stream"],
   types: ["movie", "series"],
   idPrefixes: ["tt"],
