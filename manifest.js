eb99e095188516679b242c87b9e64f47aa429d12
vanseleem <98992832+vanseleem@users.noreply.github.com>
2026-04-21 12:57:10 +0200

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,10 @@
+module.exports = {
+    id: "org.ghostream.platinum",
+    version: "1.1.0",
+    name: "Ghostream Platinum",
+    description: "Exclusive 720p/1080p high-seeder index for YTS, 1337x, and Rutracker.",
+    resources: ["stream"],
+    types: ["movie", "series"],
+    idPrefixes: ["tt"],
+    catalogs: []
+};
